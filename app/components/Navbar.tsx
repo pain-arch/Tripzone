e0f742bdf89dc2950963c1e83df8123e610a19f3
pain-arch <himulol123@gmail.com>
2024-06-30 20:09:49 +0600

    chore: Add UserNav component for user navigation in Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
+import DesktopLogo from "@/public/logo.svg";
+import MobileLogo from "@/public/mobileLogo.svg";
+import { UserNav } from "./UserNav";
 
 export function Navbar() {
     return (
         <nav className="w-full border-b">
             <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
                 <Link href="/">
-                    <Image src={ } /> 
+                    <Image
+                        src={DesktopLogo}
+                        className="w-32 hidden lg:block"
+                        alt="Desktop logo"
+                    />
+                    <Image
+                        src={MobileLogo}
+                        className="w-12 block lg:hidden"
+                        alt="Mobile logo"
+                    />
                 </Link>
+                <div className="rounded-full border px-5 py-2">
+                    <h1>Hello from the search</h1>
+                </div>
+                <UserNav />
             </div>
         </nav>
-    )
+    );
 }
